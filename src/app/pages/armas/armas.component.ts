2f587bc29314e07723d9c952bbff51f4e2f1f40a
bielmc11 <bielmarticr@gmail.com>
2023-10-09 17:41:42 +0200

    First steps with the header

---
+++ src/app/pages/armas/armas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-armas',
+  templateUrl: './armas.component.html',
+  styleUrls: ['./armas.component.scss']
+})
+export class ArmasComponent {
+
+}
