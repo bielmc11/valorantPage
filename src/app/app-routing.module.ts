13b5a3bb847eede6a95f047f63f2ff1f7a98717b
bielmc11 <bielmarticr@gmail.com>
2023-10-09 17:41:42 +0200

    First steps with the header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ArmasComponent } from './pages/armas/armas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'home', pathMatch:'full'},
+  {path: 'home', component: HomeComponent },
+  {path:'armas', component: ArmasComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
