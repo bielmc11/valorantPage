dda69f808d4e3a82156b96d9595dbe92cde1ba20
bielmc11 <bielmarticr@gmail.com>
2023-10-10 09:16:35 +0200

    day 2

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+  ancho:number = 0
+  ngOnInit(){
+    console.log(window.screenX)
+  }
+  
 }
